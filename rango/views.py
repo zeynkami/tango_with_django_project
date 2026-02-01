8036342c4872b58091b0fe28cccedb3f3a30c818
Yagmur Zeynep Acikalin <3075385a@student.gla.ac.uk>
2026-02-01 10:51:14 +0000

    Fix about page text to match exercise exactly

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ def index(request):
     return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
 
 def about(request):
-    return HttpResponse('Rango says here is about the page. <a href="/rango/">Index</a>')
+    return HttpResponse('Rango says here is about page. <a href="/rango/">Index</a>')
 
 
 
