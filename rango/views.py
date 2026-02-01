a49ab95b10229077d24388906665196ec1ec4971
Yagmur Zeynep Acikalin <3075385a@student.gla.ac.uk>
2026-02-01 21:52:34 +0000

    Added templates and media (cat image)

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse('Rango says here is about page. <a href="/rango/">Index</a>')
-
-
-
-
+    return render(request, 'rango/about.html')
