d2211face8f47937a8f638dee06a369833bbc04b
Yagmur Zeynep Acikalin <3075385a@student.gla.ac.uk>
2026-02-01 10:42:12 +0000

    Completed Rango URLs, index and about views

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 
 def index(request):
-    return Httpresponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
+    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>')
 
 def about(request):
     return HttpResponse('Rango says here is about the page. <a href="/rango/">Index</a>')
