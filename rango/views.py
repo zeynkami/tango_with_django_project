e379e284f1d981e7119302744fc930150fbe79f0
Yagmur Zeynep Acikalin <3075385a@student.gla.ac.uk>
2026-02-11 19:18:49 +0000

    chapter 8final

--- rango/views.py
+++ rango/views.py
@@ -53,7 +53,6 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
-@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -69,7 +68,6 @@ def add_category(request):
     return render(request, 'rango/add_category.html', {'form': form})
 
 
-@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
