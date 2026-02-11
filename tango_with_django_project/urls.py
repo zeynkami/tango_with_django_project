357eec0abdacf6634032e0746cf563b96c67c70c
Yagmur Zeynep Acikalin <3075385a@student.gla.ac.uk>
2026-02-11 21:18:02 +0000

    chapter 3 test changes

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'), # Fixes the index mapping error
     path('admin/', admin.site.urls),
     path('rango/', include('rango.urls')),
 ]
